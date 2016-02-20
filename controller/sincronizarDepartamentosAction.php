889b28ab365869679fef45c1299ab46ac7364c75
JudithLopez <judith.a.lopez.h@gmail.com>
2016-02-20 16:19:43 -0430

    Revert "Sincronización de kkoreal"
    This reverts commit 6d9fbd5ad0fe607fa36a5f42b3b332714f734e2b.

--- controller/sincronizarDepartamentosAction.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("../model/sincronizarDepartamentosExp.php");
-$sincronizar = new SincronizarDepartamentos();
-$resultado = $sincronizar->sincronizarDepartamentos();
-if (!empty($resultado)){
-	print_r($resultado);
-}?>
